92f3635cf3f82b399fb4a1ef026ddbca54824b77
SaraMoreiraG <sara_moreira@hotmail.es>
2023-09-15 17:13:14 +0200

    changing links

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ScrollService } from '../scroll.service';
 
 @Component({
   selector: 'about',
@@ -7,9 +6,8 @@ import { ScrollService } from '../scroll.service';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  constructor(private scrollService: ScrollService) {}
-
-  scrollTo(sectionId: string) {
-    this.scrollService.scrollToSection(sectionId);
+  openLinkInNewTab() {
+    const url = 'https://www.airbnb.es/rooms/945766271880521378?guests=1&adults=1&s=67&unique_share_id=f7ca4270-2099-4e9a-adab-1682e1fe5186';
+    window.open(url, '_blank');
   }
 }
