15b188c5d6886737474602144254f5c0845a2009
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-11 00:45:03 +0200

    uploading server

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const emailRoutes = require('./routes/emailRoutes'); // Import the emailRoutes router
 const bookingRoutes = require('./routes/bookingRoutes'); // Import the bookingRoutes router
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,12 @@ app.use(express.json());
 app.use('/email', emailRoutes);
 // Use the bookingRoutes router for booking-related endpoints
 app.use('/bookings', bookingRoutes);
+
+//Base path to the angular dist folder
+app.use(express.static(path.join(__dirname, 'dist/casa-rural')))
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'dist/casa-rural/index.html'))
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor en ejecución en el puerto ' + (process.env.PORT || 3000));
