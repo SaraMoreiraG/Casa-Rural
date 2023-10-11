a7864d722fa3187cb80e2de41e470f7cf0100327
SaraMoreiraG <sara_moreira@hotmail.es>
2023-10-11 14:30:02 +0200

    ready to deploy

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'testimonials',
@@ -7,10 +6,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./testimonials.component.css']
 })
 export class TestimonialsComponent {
-
-  constructor(private router: Router) { }
-
-  navigateToBookings() {
-    this.router.navigate(['/booking']);
-  }
 }
